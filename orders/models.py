0a74a6388463a83dc46624622b204d4d4e7e2af3
rusrom <rusrom@guyfawkes.33mail.com>
2019-10-02 16:27:59 +0300

    Signal for calculating Order total

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from cart.models import Cart
 from ecommerce.utils import unique_order_id_generator
@@ -22,6 +22,10 @@ class Order(models.Model):
     shipping_total = models.DecimalField(max_digits=10, decimal_places=2, default=50.00)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
+    def update_total(self):
+        self.total = self.cart.total + self.shipping_total
+        self.save()
+
     def __str__(self):
         return self.order_id
 
@@ -33,4 +37,21 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
 
+
 # Generate the order TOTAL
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:
+        cart = instance
+        qs = Order.objects.filter(cart__id=cart.id)
+        if qs.count() == 1:
+            order = qs.first()
+            order.update_total()
+post_save.connect(post_save_cart_total, Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    print('running post_save_order')
+    if created:
+        print('Updating first ...')
+        instance.update_total()
+post_save.connect(post_save_order, sender=Order)
